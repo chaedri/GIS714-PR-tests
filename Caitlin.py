203c0fffadf5c0b6158d1ac467cc2845f1ece72b
Caitlin H <caitlin.haedrich@gmail.com>
2026-03-25 16:10:32 -0400

    Update with year

--- Caitlin.py
+++ Caitlin.py
@@ -1,4 +1,4 @@
 print("Howdy! I'm Caitlin, the TA for GIS714.")
 print("What's up! This is Caitlin again, I love git.")
 print("This is Caitlin yet again, still love git.")
-print("Hola! This is now Dr. Caitlin!")
+print("Hola! This is now Dr. Caitlin in 2026!")
